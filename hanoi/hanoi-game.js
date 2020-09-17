19f27b2ea37b636ea17ab51c7166ecd518c09609
Ezra <ezpinsky@gmail.com>
2020-09-17 17:09:22 -0400

    completed all specs GAME WORKS!

--- hanoi/hanoi-game.js
+++ hanoi/hanoi-game.js
@@ -1,78 +1,85 @@
 class HanoiGame {
-  constructor(towers = [[3, 2, 1], [], []]) {
-    this.towers = towers;
-  }
+	constructor(towers = [[3, 2, 1], [], []]) {
+		this.towers = towers;
+	}
 
-  isValidMove(startTowerIdx, endTowerIdx) {
-    const startTower = this.towers[startTowerIdx];
-    const endTower = this.towers[endTowerIdx];
+	isValidMove(startTowerIdx, endTowerIdx) {
+		const startTower = this.towers[startTowerIdx];
+		const endTower = this.towers[endTowerIdx];
 
-    if (startTower === undefined) {
-      return false;
-    } else if (startTower.length === 0) {
-      return false;
-    } else if (startTowerIdx === endTowerIdx) {
-      return false;
-    } else if (endTower.length === 0) {
-      return true;
-    } else if (
-      startTower[startTower.length - 1] < endTower[endTower.length - 1]
-    ) {
-      return true;
-    } else if (
-      startTower[startTower.length - 1] > endTower[endTower.length - 1]
-    ) {
-      return false;
-    }
-  }
+		if (startTower === undefined || endTower === undefined) {
+			return false;
+		} else if (startTower.length === 0) {
+			return false;
+		} else if (startTowerIdx === endTowerIdx) {
+			return false;
+		} else if (endTower.length === 0) {
+			return true;
+		} else if (startTower[startTower.length - 1] < endTower[endTower.length - 1]) {
+			return true;
+		} else if (startTower[startTower.length - 1] > endTower[endTower.length - 1]) {
+			return false;
+		}
+	}
 
-  move(startTowerIdx, endTowerIdx) {
-    const startTower = this.towers[startTowerIdx];
-    const endTower = this.towers[endTowerIdx];
+	move(startTowerIdx, endTowerIdx) {
+		const startTower = this.towers[startTowerIdx];
+		const endTower = this.towers[endTowerIdx];
 
-    if (this.isValidMove(startTowerIdx, endTowerIdx)) {
-      startTower.pop();
-    }
-  }
+		if (this.isValidMove(startTowerIdx, endTowerIdx)) {
+			endTower.push(startTower.pop());
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  isWon() {}
+	isWon() {
+		if (this.towers[1].length === 3) {
+			return true;
+		} else if (this.towers[2].length === 3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  // the below methods are complete and do not need to be modified
-  print() {
-    // will print our board nicely to our user
-    console.log(JSON.stringify(this.towers));
-  }
+	// the below methods are complete and do not need to be modified
+	print() {
+		// will print our board nicely to our user
+		console.log(JSON.stringify(this.towers));
+	}
 
-  promptMove(reader, callback) {
-    this.print();
-    reader.question("Enter a starting tower: ", (start) => {
-      const startTowerIdx = parseInt(start);
-      reader.question("Enter an ending tower: ", (end) => {
-        const endTowerIdx = parseInt(end);
-        callback(startTowerIdx, endTowerIdx);
-      });
-    });
-  }
+	promptMove(reader, callback) {
+		this.print();
+		reader.question("Enter a starting tower: ", (start) => {
+			const startTowerIdx = parseInt(start);
+			reader.question("Enter an ending tower: ", (end) => {
+				const endTowerIdx = parseInt(end);
+				callback(startTowerIdx, endTowerIdx);
+			});
+		});
+	}
 
-  run(reader, callback) {
-    // we will prompt our user to provide a start and stop index using
-    // a readline interface
-    this.promptMove(reader, (startTowerIdx, endTowerIdx) => {
-      // if the move is invalid we tell the user
-      if (!this.move(startTowerIdx, endTowerIdx)) {
-        console.log("Invalid move!");
-      }
+	run(reader, callback) {
+		// we will prompt our user to provide a start and stop index using
+		// a readline interface
+		this.promptMove(reader, (startTowerIdx, endTowerIdx) => {
+			// if the move is invalid we tell the user
+			if (!this.move(startTowerIdx, endTowerIdx)) {
+				console.log("Invalid move!");
+			}
 
-      if (!this.isWon()) {
-        // Continue to play!
-        this.run(reader, callback);
-      } else {
-        this.print();
-        console.log("You win!");
-        callback();
-      }
-    });
-  }
+			if (!this.isWon()) {
+				// Continue to play!
+				this.run(reader, callback);
+			} else {
+				this.print();
+				console.log("You win!");
+				callback();
+			}
+		});
+	}
 }
 
 module.exports = HanoiGame;
